f0f0ab5846a26a0dbd02af69fdb59a495435ed81
Amir <amircodes10@gmail.com>
2022-12-25 22:42:55 +0330

    Modified docker related stuff

--- src/ecommerce/settings.py
+++ src/ecommerce/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-f019$rn8ksnti&b)z(&ia!_+67$biv^-k6hiqj8uxsa47biltp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["0.0.0.0", "localhost"]
 
 
 # Application definition
--- src/search/api/views.py
+++ src/search/api/views.py
@@ -1,4 +1,4 @@
-from rest_framework import status
+from rest_framework import status, response
 from rest_framework.views import APIView
 from elasticsearch_dsl import Q
 from rest_framework.pagination import LimitOffsetPagination
@@ -8,7 +8,7 @@ from search.documents import ProductInventoryDocument
 
 
 class SearchProductInventoryAPIView(APIView, LimitOffsetPagination):
-    serializer = ProductInventorySearchSerializer
+    product_inventory_serializer = ProductInventorySearchSerializer
     search_els_document = ProductInventoryDocument
 
     def get(self, request, query, *args, **kwargs):
@@ -19,14 +19,15 @@ class SearchProductInventoryAPIView(APIView, LimitOffsetPagination):
                 fields=["product.name", "product.web_id", "brand.name"],
                 fuzziness="auto",
             ) & Q(
+                "bool",
                 should=[Q("match", is_default=True)],
                 minimum_should_match=1,
             )
             search = self.search_els_document.search().query(els_q)
-            response = search.execute()
+            search_response = search.execute()
 
-            result = self.paginate_queryset(response, request, view=self)
-            serializer = self.serializer(result, many=True)
+            result = self.paginate_queryset(search_response, request, view=self)
+            serializer = self.product_inventory_serializer(result, many=True)
             return self.get_paginated_response(serializer.data)
 
         except Exception as e:
